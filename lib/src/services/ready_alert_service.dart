cae4213ff193c1c64fb72416e4ce27048c141c54
Akshit <akshitcasual1@gmail.com>
2026-05-10 05:25:14 +0530

    fix ready alert web interop

--- lib/src/services/ready_alert_service.dart
+++ lib/src/services/ready_alert_service.dart
@@ -1,6 +1,8 @@
-import 'package:flutter/services.dart';
-import 'package:flutter/foundation.dart';
 import 'dart:js_interop';
+import 'dart:js_interop_unsafe';
+
+import 'package:flutter/foundation.dart';
+import 'package:flutter/services.dart';
 import 'package:vibration/vibration.dart';
 
 class ReadyAlertService {
@@ -8,20 +10,21 @@ class ReadyAlertService {
     if (!kIsWeb) {
       return true;
     }
-    final alerts = globalContext['bawarchiiAlerts'];
-    final requestPermission = alerts?['requestPermission'];
+    final alerts = globalContext.getProperty<JSObject?>('bawarchiiAlerts'.toJS);
+    final requestPermission =
+        alerts?.getProperty<JSFunction?>('requestPermission'.toJS);
     if (requestPermission == null) {
       return false;
     }
-    final result =
-        await (requestPermission as JSFunction).callAsFunction().toDart;
+    final promise = requestPermission.callAsFunction() as JSPromise<JSAny?>;
+    final result = await promise.toDart;
     return result.dartify() == 'granted';
   }
 
   Future<void> buzz() async {
     _notifyReady();
     _playReadySound();
-    final hasVibrator = await Vibration.hasVibrator() ?? false;
+    final hasVibrator = await Vibration.hasVibrator();
     if (hasVibrator) {
       await Vibration.vibrate(pattern: [0, 240, 120, 240, 120, 320]);
       return;
@@ -33,8 +36,10 @@ class ReadyAlertService {
     if (!kIsWeb) {
       return;
     }
-    final notifyReady = globalContext['bawarchiiAlerts']?['notifyReady'];
-    if (notifyReady is JSFunction) {
+    final notifyReady = globalContext
+        .getProperty<JSObject?>('bawarchiiAlerts'.toJS)
+        ?.getProperty<JSFunction?>('notifyReady'.toJS);
+    if (notifyReady != null) {
       notifyReady.callAsFunction();
     }
   }
@@ -44,8 +49,10 @@ class ReadyAlertService {
       SystemSound.play(SystemSoundType.alert);
       return;
     }
-    final playReadySound = globalContext['bawarchiiAlerts']?['playReadySound'];
-    if (playReadySound is JSFunction) {
+    final playReadySound = globalContext
+        .getProperty<JSObject?>('bawarchiiAlerts'.toJS)
+        ?.getProperty<JSFunction?>('playReadySound'.toJS);
+    if (playReadySound != null) {
       playReadySound.callAsFunction();
     }
   }
